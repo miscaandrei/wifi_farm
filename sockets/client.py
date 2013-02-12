21d2d4e7c1a12aec103661a1934d8ddd824bb927
Andrei Misca <miscaandrei@gmail.com>
2013-02-12 20:26:36 +0100

    Reorganitzacio de fitxers en una carpeta central src

--- sockets/client.py
+++
@@ -1,6 +0,0 @@
-import socket
-dgramSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-dgramSock.sendto("Hello World\n", ('192.168.0.200',23000))
-print dgramSock.recv(100)
-dgramSock.close()
